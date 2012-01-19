c8a01a457764e4ed5d196196182afac4a0ceb6c6
jp.berdel <jp.berdel@googlemail.com>
2012-01-19 13:38:33 +0100

    fixed minor "stop then dispose" nullref

--- src/Niob/NiobServer.cs
+++ src/Niob/NiobServer.cs
@@ -66,7 +66,7 @@ namespace Niob
 
             _disposed = true;
 
-            if (!_stopSource.IsCancellationRequested)
+            if (_stopSource != null && !_stopSource.IsCancellationRequested)
             {
                 Stop();
             }
